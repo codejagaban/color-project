da851f2bdaeda155e71cafb538d9e5bbe3b04989
unknown <okpusjamin@gmail.com>
2018-11-07 12:59:37 -0800

    grab color clicked square

--- js/colorGame.js
+++ js/colorGame.js
@@ -1,3 +1,4 @@
+// creates an Array of colors
 var colors = [
     "rgb(255, 0, 0)",
     "rgb(255, 255, 0)",
@@ -6,11 +7,32 @@ var colors = [
     "rgb(0, 0, 255)",
     "rgb(255, 0, 255)"
 ],
+
+// gets elements from the DOM Tree
 squares = document.querySelectorAll(".square"),
-pickedColor = colors[3],
 colorDisplay = document.getElementById("colorDisplay");
+pickedColor = colors[3],
 colorDisplay.textContent = pickedColor;
+
 for(var i = 0; i < squares.length; i++) {
+
+    // add initial colors to each of the squares
     squares[i].style.backgroundColor = colors[i];
+
+    // add click listeners to each of the squares
+    squares[i].addEventListener("click", function(){
+        // grab color of clicked square
+        var clickedColor =  this.style.backgroundColor;
+
+        // compare color to pickedColor
+        if (clickedColor === pickedColor) {
+            alert("CORRECT")
+            
+        } else {
+            this.style.backgroundColor = "#232323"
+            
+        }
+    })
+
 
 }
